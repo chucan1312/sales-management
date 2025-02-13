c381a97c66f2ae719afe96aa47d60abea6815e0b
An <trchan@students.cs.ubc.ca>
2025-02-13 12:59:54 -0800

    Added new method to PurchasesRecord

--- src/main/model/PurchaseRecord.java
+++ src/main/model/PurchaseRecord.java
@@ -13,19 +13,30 @@ public class PurchaseRecord {
         //stub
     }
 
-    // EFFECTS: add a purchase to purchases
+    // EFFECTS: add a purchase to purchases and add actualPaidAmount to total
     public void addPurchase(Purchase purchase) {
-        // stub
+        //stub
     }
 
-    // EFFECTS: return a list of all purchases for one day
-    public List<Purchase> getOneDayPurchases(LocalDate date) {
+    // EFFECTS: return a list of all purchases from start date to end date
+    public List<Purchase> getPurchasesBetween(LocalDate start, LocalDate end) {
         return null; //stub
     }
 
     // EFFECTS: return a list of all unreviewed purchases for one day with given payment method
     public List<Purchase> getOneDayPurchasesMethod(LocalDate date, String method) {
         return null; //stub
+    }
+
+    // EFFECTS: return the profit from start date to end date 
+    public Double getProfit(LocalDate start, LocalDate end) {
+        return -1.1; //stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: clear purchases record
+    public void clearPurchase() {
+        //stub
     }
 
     public List<Purchase> getPurchases() {
