110ce5c48eb6ac17e0653875d87721f32782c09d
An <trchan@students.cs.ubc.ca>
2025-03-01 11:31:18 -0800

    Specified findProduct() in Inventory

--- src/main/model/Inventory.java
+++ src/main/model/Inventory.java
@@ -12,6 +12,11 @@ public class Inventory {
         products = new ArrayList<Product>();
     }
 
+    // EFFECTS: return list of product
+    public List<Product> findProduct() {
+        //stub
+    }
+
     // EFFECTS: return a product when given an id,
     // return null if not found
     public Product findProductWithId(String id) {
