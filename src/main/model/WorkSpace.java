6a74f313682fc06f4799f5004eaf68ac58d5cdeb
An <trchan@students.cs.ubc.ca>
2025-03-14 00:33:22 -0700

    Specified WorkSpace

---
+++ src/main/model/WorkSpace.java
@@ -0,0 +1,12 @@
+package model;
+
+// Represent a workspace with an inventory and a purchase record
+public class WorkSpace {
+    private Inventory i;
+    private PurchaseRecord pr;
+
+    // EFFECTS: Construct a workspace with a new inventory and purchase record
+    public WorkSpace() {
+        // stub;
+    }
+}
