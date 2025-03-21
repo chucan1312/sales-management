18a6ad956bd7f63c0e43f1cd078197bf307fb663
An <trchan@students.cs.ubc.ca>
2025-03-21 13:08:28 -0700

    Add test for JsonReader

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,9 +16,9 @@ class JsonReaderTest extends JsonTest {
     @Test
     void testInventoryReaderInvalidFile() {
         try {
-            Inventory i = new Inventory();
+            WorkSpace ws = new WorkSpace();
             JsonReader reader = new JsonReader("./data/noSuchFile.json");
-            reader.readInventory();
+            ws = reader.read();
             fail("IOException was expected");
         } catch (IOException e) {
             //pass
@@ -28,8 +29,10 @@ class JsonReaderTest extends JsonTest {
     void testReaderEmptyInventory() {
         JsonReader reader = new JsonReader("./data/testReaderEmpty");
         try {
-            Inventory i = reader.readInventory();
-            assertEquals(0, i.getProducts().size());
+            WorkSpace ws = new WorkSpace();
+            ws = reader.read();
+            assertTrue(ws.getInventory().getProducts().isEmpty());
+            assertTrue(ws.getPurchaseRecord().getPurchases().isEmpty());
         } catch (IOException e) {
             fail("Couldn't read from file");
         }
@@ -39,10 +42,24 @@ class JsonReaderTest extends JsonTest {
     void testReaderGeneralInventory() {
         JsonReader reader = new JsonReader("./data/testReaderGeneral");
         try {
-            Inventory i = reader.readInventory();
-            assertEquals(2, i.getProducts().size());
-            checkProduct("cake", "123", 12.5, 15.0, 12, i.getProducts().get(0));
-            checkProduct("cookie", "456", 5.2, 6.0, 8, i.getProducts().get(1));
+            WorkSpace ws = new WorkSpace();
+            ws = reader.read();
+            List<Product> products = ws.getInventory().getProducts();
+            List<Purchase> purchases = ws.getPurchaseRecord().getPurchases();
+
+            assertEquals(2, products.size());
+            checkProduct("cake", "123", 12.5, 15.0, 12, products.get(0));
+            checkProduct("cookie", "456", 5.2, 6.0, 8, products.get(1));
+
+            assertEquals(2, purchases.size());
+            LocalDate date1 = LocalDate.of(2024, 12, 13);
+            LocalDate date2 = LocalDate.of(2025, 3, 14);
+            Map<Product, Integer> purchased1 = new HashMap<>();
+            purchased1.put(products.get(0), 1);
+            purchased1.put(products.get(1), 2);
+            Map<Product, Integer> purchased2 = new HashMap<>();
+            checkPurchase(date1, purchased1, 24.0, "Cash", true, purchases.get(0));
+            checkPurchase(date2, purchased2, 7.0, "E-transfer", false, purchases.get(1));
         } catch (IOException e) {
             fail("Couldn't read from file");
         }
