51c4ba701d3b292930f1fb806d3303b4be74659e
An <trchan@students.cs.ubc.ca>
2025-03-21 13:08:13 -0700

    Override hashcode and equals in Product

--- src/main/model/Product.java
+++ src/main/model/Product.java
@@ -88,4 +88,39 @@ public class Product implements Writable {
         return json;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((sellingPrice == null) ? 0 : sellingPrice.hashCode());
+        result = prime * result + ((unitPrice == null) ? 0 : unitPrice.hashCode());
+        result = prime * result + quantity;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    
 }
