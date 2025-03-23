e3a5e62100a03277b0ac90e61410f50a0ee51cc4
An <trchan@students.cs.ubc.ca>
2025-03-22 21:19:38 -0700

    Specifies AddProductMenu

---
+++ src/main/ui/menu/AddProductMenu.java
@@ -0,0 +1,22 @@
+package ui.menu;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+import model.*;
+
+public class AddProductMenu extends JFrame implements ActionListener {
+    private WorkSpace workSpace;
+
+    public AddProductMenu(WorkSpace workSpace) {
+        this.workSpace = workSpace;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
+}
