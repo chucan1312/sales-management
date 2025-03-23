6cba584fa3b2329ae7094781c9a9bb2fed37fab5
An <trchan@students.cs.ubc.ca>
2025-03-23 03:40:28 -0700

    Specifies InventoryMenu

---
+++ src/main/ui/menu/InventoryMenu.java
@@ -0,0 +1,30 @@
+package ui.menu;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+import model.*;
+
+public class InventoryMenu extends JFrame implements ActionListener {
+
+    // EFFECTS: creates a new Inventory Menu with workspace
+    public void InventoryMenu(WorkSpace workSpace) {
+        // stub
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+
+    // EFFECTS: instantiates a new button and add it to the content pane
+    private void addButton(String command) {
+    }
+
+    // EFFECTS: parses the data from Inventory into the JTable
+    private void parseInventory(Inventory i, JTable table) {
+
+    }
+}
